fd86394752c47008013322fa70c180c822bd81e8
Dustin Myers <dustinblaine.myers@gmail.com>
2020-03-11 10:47:04 -0400

    Updated Climber Model

--- models/climber.js
+++ models/climber.js
@@ -1,53 +1,51 @@
 const db = require('./conn');
-bcrypt = require('bcryptjs');
+bcrypt = require('bcryptjs')
+
 
 class Climber {
-    constructor(id, name, email, password) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
+  constructor(id, first_name, last_name, email, password) {
+    this.id = id;
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.password = password;
+  }
+
+  checkPassword(hashedPassword) {
+    return bcrypt.compareSync(this.password, hashedPassword);
+  }
+
+  async addUser() {
+    try {
+      const response = await db.one(
+        `INSERT INTO climber (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING id;`,
+        [this.first_name, this.last_name, this.email, this.password]
+      );
+      return response;
+    } catch (error) {
+      console.error('ERROR: ', error);
+      return error;
     }
+  }
 
-    checkPassword(hashedPassword) {
-        return bcrypt.compareSync(this.password, hashedPassword);
-    } 
-
-    async addUser() {
-        console.log('adding user', this.name);
-        try {
-            const response = await db.one(
-                `INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id`,
-                [this.name, this.email, this.password]
-            );
-            return response;
-        } catch(error) {
-        console.error("ERROR: ", error);
-        return error;
+  async loginUser() {
+    try {
+      const response = await db.one(
+        `SELECT id, first_name, last_name, password FROM users WHERE email = $1;`,
+        [this.email]
+      );
+      const isValid = this.checkPassword(response.password);
+      if (!!isValid) {
+        console.log('SUCCESS!!!!', isValid);
+      } else {
+        console.log('GO AWAY!!!!!', isValid);
+      }
+      return isValid;
+    } catch (error) {
+      console.error('ERROR: ', error);
+      return error;
     }
-}
-
-    async loginUser() {
-        console.log('logging in user');
-        try {
-            const response = await db.one(
-                `SELECT id, name, password FROM user WHERE email = $1;`, 
-                [this.email]
-            );
-
-            const isValid = this.checkPassword(response.password);
-
-            if (!!isValid) {
-                const { id, name } = response;
-                return { isValid, id, name };
-            } else {
-                return {isValid};
-            }
-        } catch(error) {
-            console.error("ERROR: ", error);
-            return error;
-        }
-    }
+  }
 }
 
 module.exports = Climber;
